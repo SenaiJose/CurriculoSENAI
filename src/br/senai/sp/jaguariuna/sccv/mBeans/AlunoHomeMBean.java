df7a312c242c8d695f7dd27941528db5a37ec88d
José Siqueira <jose.eduardo.ss@hotmail.com>
2018-09-03 13:38:38 -0300

    commit
    alunohomeMBean

--- src/br/senai/sp/jaguariuna/sccv/mBeans/AlunoHomeMBean.java
+++ src/br/senai/sp/jaguariuna/sccv/mBeans/AlunoHomeMBean.java
@@ -1,9 +1,27 @@
 package br.senai.sp.jaguariuna.sccv.mBeans;
 
+import java.sql.SQLException;
+
 import javax.faces.bean.ManagedBean;
+
+import br.senai.sp.jaguariuna.sccv.entities.Curriculo;
+import br.senai.sp.jaguariuna.sccv.uDao.CurriculoDao;
 
 @ManagedBean
 public class AlunoHomeMBean {
+	
+	CurriculoDao curriculoDao;
+	Curriculo curriculo;
+	
+	public AlunoHomeMBean() {
+		curriculo = new Curriculo();
+		curriculoDao = new CurriculoDao();
+		
+	}
+	
+	public void listarCurriculo() throws SQLException {
+		curriculo = curriculoDao.listarCurriculo(curriculo.getCurso());
+	}
 	
 	
 
