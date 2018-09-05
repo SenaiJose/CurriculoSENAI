1b9824305c5461c4212e8ec41b3faf1dd03746cf
José Siqueira <jose.eduardo.ss@hotmail.com>
2018-09-05 11:14:59 -0300

    Commit
    AlunoHomeMBean

--- src/br/senai/sp/jaguariuna/sccv/mBeans/AlunoHomeMBean.java
+++ src/br/senai/sp/jaguariuna/sccv/mBeans/AlunoHomeMBean.java
@@ -1,28 +1,59 @@
 package br.senai.sp.jaguariuna.sccv.mBeans;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
 
 import br.senai.sp.jaguariuna.sccv.entities.Curriculo;
+import br.senai.sp.jaguariuna.sccv.entities.Usuario;
 import br.senai.sp.jaguariuna.sccv.uDao.CurriculoDao;
 
 @ManagedBean
+@ViewScoped
 public class AlunoHomeMBean {
 	
 	CurriculoDao curriculoDao;
-	Curriculo curriculo;
+	List<Curriculo> listarCurriculo;
 	
 	public AlunoHomeMBean() {
-		curriculo = new Curriculo();
 		curriculoDao = new CurriculoDao();
-		
+		listarCurriculo = new ArrayList<Curriculo>();
+		System.out.println("Pegou 65465465");
+
 	}
 	
-	public void listarCurriculo() throws SQLException {
-		curriculo = curriculoDao.listarCurriculo(curriculo.getCurso());
+	@ManagedProperty(value = "#{alunoIndexMBean}")
+	private AlunoIndexMBean alunoIndexMBean;
+
+	public void setAlunoIndexMBean(AlunoIndexMBean alunoIndexMBean) {
+		this.alunoIndexMBean = alunoIndexMBean;
+		System.out.println("Pegou usuario");
 	}
 	
-	
+	@PostConstruct
+	public void postConstruct() {
+		Usuario u = alunoIndexMBean.getUsuario();
+		try {
+			listarCurriculo = curriculoDao.listarCurriculo(u.getId());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(listarCurriculo.size());
+		System.out.println("Teste");
+	}
+
+	public List<Curriculo> getListarCurriculo() {
+		return listarCurriculo;
+	}
+
+	public void setListarCurriculo(List<Curriculo> listarCurriculo) {
+		this.listarCurriculo = listarCurriculo;
+	}
 
 }
--- src/br/senai/sp/jaguariuna/sccv/uDao/CurriculoDao.java
+++ src/br/senai/sp/jaguariuna/sccv/uDao/CurriculoDao.java
@@ -11,37 +11,37 @@ import br.senai.sp.jaguariuna.sccv.entities.Curriculo;
 import br.senai.sp.jaguariuna.sccv.jdbc.ConnectionDB;
 
 public class CurriculoDao {
-	
+
 	private Connection conn;
-	
+
 	public CurriculoDao() {
 		conn = ConnectionDB.getConnection();
 	}
-	
-	public List<Curriculo> listarCurriculo (String id_usuario) throws SQLException {
-		String sql = "SELECT curri.data_criacao, tur.nome AS nomeTurma, cur.nome AS nomeCurso, curri.semestre FROM curriculum_vitae AS curri"
-				+ "INNER JOIN turma AS tur ON tur.id = curri.id_turma "
-				+ "INNER JOIN curso AS cur ON cur.id = curri.id_curso "
-				+ "WHERE curri.id_usuario = 1;" ;
-				
-				
-				 
-		
+
+	public List<Curriculo> listarCurriculo(Integer id_usuario) throws SQLException {
+		String sql = "SELECT curriculum_vitae.*, tur.nome AS nomeTurma, cur.nome AS nomeCurso, curriculum_vitae.semestre FROM curriculum_vitae "
+				+ "INNER JOIN turma AS tur ON tur.id = curriculum_vitae.id_turma "
+				+ "INNER JOIN curso AS cur ON cur.id = curriculum_vitae.id_curso " + "WHERE curriculum_vitae.id_usuario = ?;";
+
 		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, id_usuario);
-		
+
+		ps.setInt(1, id_usuario);
+
 		ResultSet rs = ps.executeQuery();
 		List<Curriculo> lista = new ArrayList<Curriculo>();
-		while(rs.next()) {
+
+		while (rs.next()) {
 			Curriculo c = new Curriculo();
-			
+
 			c.getTurma().setNome(rs.getString("nomeTurma"));
 			c.getCurso().setNome(rs.getString("nomeCurso"));
 			c.setData_criacao(rs.getInt("data_criacao"));
+			
+			lista.add(c);
 		}
-		
+
 		return lista;
-		
+
 	}
 
 }
